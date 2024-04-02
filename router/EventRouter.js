46ac1838f98c56365d4315b57c939f83655893ae
Abhinav saini <54369733+vishus66@users.noreply.github.com>
2024-04-02 19:35:38 +0530

    EventRouter.js

--- router/EventRouter.js
+++ router/EventRouter.js
@@ -1,9 +1,11 @@
+// Import the Router class in the Express js  
 const router = require("express").Router()
+// function is import and distructure  form the EventController module 
 const{createRecorde,getRecord,findevent} = require("../controller/EventController")
-
+//Each router tell the functionality that to get ,post....
 router.get("/",getRecord)
 router.post("/",createRecorde)
 router.get("/find",findevent)
 
-
+// Export the file 
 module.exports = router
